b295b346eb8fa719ccc5f2791b34312994c2a39e
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2019-10-02 14:57:37 +0900

    写真を外部ストレージ領域に保存

--- app/src/main/java/com/example/plank/MainActivity.java
+++ app/src/main/java/com/example/plank/MainActivity.java
@@ -1,6 +1,26 @@
 package com.example.plank;
 
 import androidx.appcompat.app.AppCompatActivity;
+//追加
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+//ここまで
+
 
 import android.os.Bundle;
 
@@ -16,11 +36,15 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private final static int RESULT_CAMERA = 1001;
+    private final static int REQUEST_PERMISSION = 1002;//10/2追加
     private ImageView imageView;
+    private Uri cameraUri;//10/2追加
+    private File cameraFile;//10/2追加
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("debug","onCreate()");//10/2追加
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.image_view);
 
@@ -28,16 +52,48 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, RESULT_CAMERA);
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//10/2コメントアウト
+                //startActivityForResult(intent, RESULT_CAMERA);//10/2コメントアウト
+                //追加
+                // Android 6, API 23以上でパーミッシンの確認
+                if (Build.VERSION.SDK_INT >= 23) {
+                    checkPermission();
+                }
+                else {
+                    cameraIntent();//下に関数作成10/2
+                }
+                //ここまで
             }
         });
     }
+    //10/2作成　カメラ保存
+    private void cameraIntent(){
+        // 保存先のフォルダー
+        File cFolder = getExternalFilesDir(Environment.DIRECTORY_DCIM);
 
+        String fileDate = new SimpleDateFormat(
+                "ddHHmmss", Locale.US).format(new Date());
+        // ファイル名
+        String fileName = String.format("CameraIntent_%s.jpg", fileDate);
 
+        cameraFile = new File(cFolder, fileName);
+
+        cameraUri = FileProvider.getUriForFile(
+                MainActivity.this,
+                getApplicationContext().getPackageName() + ".fileprovider",
+                cameraFile);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, cameraUri);
+        startActivityForResult(intent, RESULT_CAMERA);
+
+        Log.d("debug","startActivityForResult()");
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //下10/2コメントアウト
+        /**
         if (requestCode == RESULT_CAMERA) {
             Bitmap bitmap;
             // cancelしたケースも含む
@@ -55,8 +111,87 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("debug",String.format("h= %d",bmpHeight));
                 }
             }
+            */
+        if (requestCode == RESULT_CAMERA) {
 
-            imageView.setImageBitmap(bitmap);
+            if(cameraUri != null){
+                imageView.setImageURI(cameraUri);
+
+                registerDatabase(cameraFile);
+            }
+            else{
+                Log.d("debug","cameraUri == null");
+            }
+
+            //imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    //以下の関数10/2追加
+    // アンドロイドのデータベースへ登録する
+    private void registerDatabase(File file) {
+        ContentValues contentValues = new ContentValues();
+        ContentResolver contentResolver = MainActivity.this.getContentResolver();
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put("_data", file.getAbsolutePath());
+        contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues);
+    }
+
+    // Runtime Permission check
+    private void checkPermission(){
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED){
+            cameraIntent();
+        }
+        // 拒否していた場合
+        else{
+            requestPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+
+        } else {
+            Toast toast = Toast.makeText(this,
+                    "許可されないとアプリが実行できません",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,},
+                    REQUEST_PERMISSION);
+
+        }
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        Log.d("debug","onRequestPermissionsResult()");
+
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                cameraIntent();
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this,
+                        "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
     }
 }
