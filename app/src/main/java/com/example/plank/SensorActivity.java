b79731e22af04f3e483a9bb05b3df065f48db65a
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2019-10-10 11:17:27 +0900

    加速度センサーの表示

--- app/src/main/java/com/example/plank/SensorActivity.java
+++ app/src/main/java/com/example/plank/SensorActivity.java
@@ -1,20 +1,34 @@
 package com.example.plank;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
+//import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import java.util.List;
 
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
 
-public class SensorActivity extends AppCompatActivity {
+    private SensorManager sensorManager;
+    private TextView textView, textInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_sensor);
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        textInfo = findViewById(R.id.text_info);
+
+        // Get an instance of the TextView
+        textView = findViewById(R.id.text_view);
 
         Button returnButton_sensor = findViewById(R.id.return_button_sensor);
         returnButton_sensor.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +37,117 @@ public class SensorActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Listenerの登録
+        Sensor accel = sensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+        //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME);
+        //sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_UI);
+    }
+
+
+    // 解除するコードも入れる!
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Listenerを解除
+        sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float sensorX, sensorY, sensorZ;
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            sensorX = event.values[0];
+            sensorY = event.values[1];
+            sensorZ = event.values[2];
+
+            String strTmp = "加速度センサー\n"
+                    + " X: " + sensorX + "\n"
+                    + " Y: " + sensorY + "\n"
+                    + " Z: " + sensorZ;
+            textView.setText(strTmp);
+
+            showInfo(event);
+        }
+    }
+
+    // （お好みで）加速度センサーの各種情報を表示
+    private void showInfo(SensorEvent event){
+        // センサー名
+        StringBuffer info = new StringBuffer("Name: ");
+        info.append(event.sensor.getName());
+        info.append("\n");
+
+        // ベンダー名
+        info.append("Vendor: ");
+        info.append(event.sensor.getVendor());
+        info.append("\n");
+
+        // 型番
+        info.append("Type: ");
+        info.append(event.sensor.getType());
+        info.append("\n");
+
+        // 最小遅れ
+        int data = event.sensor.getMinDelay();
+        info.append("Mindelay: ");
+        info.append(String.valueOf(data));
+        info.append(" usec\n");
+
+        // 最大遅れ
+        //data = event.sensor.getMaxDelay();
+        info.append("Maxdelay: ");
+        info.append(String.valueOf(data));
+        info.append(" usec\n");
+
+        // レポートモード
+        //data = event.sensor.getReportingMode();
+        String stinfo = "unknown";
+        if(data == 0){
+            stinfo = "REPORTING_MODE_CONTINUOUS";
+        }else if(data == 1){
+            stinfo = "REPORTING_MODE_ON_CHANGE";
+        }else if(data == 2){
+            stinfo = "REPORTING_MODE_ONE_SHOT";
+        }
+        info.append("ReportingMode: ");
+        info.append(stinfo);
+        info.append("\n");
+
+        // 最大レンジ
+        info.append("MaxRange: ");
+        float fData = event.sensor.getMaximumRange();
+        info.append(String.valueOf(fData));
+        info.append("\n");
+
+        // 分解能
+        info.append("Resolution: ");
+        fData = event.sensor.getResolution();
+        info.append(String.valueOf(fData));
+        info.append(" m/s^2\n");
+
+        // 消費電流
+        info.append("Power: ");
+        fData = event.sensor.getPower();
+        info.append(String.valueOf(fData));
+        info.append(" mA\n");
+        textInfo.setText(info);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
