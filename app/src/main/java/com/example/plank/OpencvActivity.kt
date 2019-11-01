14e67783c2a1723f204db0824f57c4aaae5237f5
Kouki Nishihara <e175774@ie.u-ryukyu.ac.jp>
2019-11-02 07:45:52 +0900

    OpenCVのバージョンを表示できるようになった

--- app/src/main/java/com/example/plank/OpencvActivity.kt
+++ app/src/main/java/com/example/plank/OpencvActivity.kt
@@ -1,10 +1,13 @@
 package com.example.plank
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-
 import androidx.appcompat.app.AppCompatActivity
+import org.opencv.android.OpenCVLoader
+import android.widget.TextView
+
+//OpenCVのセットアップは以下のリンク参照
+//https://algorithm.joho.info/programming/kotlin/opencv-install-kt/
 
 class OpencvActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +17,13 @@ class OpencvActivity : AppCompatActivity() {
         val returnButton = findViewById<Button>(R.id.return_sub_open)
         returnButton.setOnClickListener { finish() }
 
-
+        //OpenCVの動作確認用にバージョンを表示する
+        val txtVersion : TextView = findViewById(R.id.version)
+        if (OpenCVLoader.initDebug()) {
+            txtVersion.text = "OpenCV Version: " + OpenCVLoader.OPENCV_VERSION
+        } else {
+            txtVersion.text = "OpenCV Version: Not found."
+        }
     }
 
 }
