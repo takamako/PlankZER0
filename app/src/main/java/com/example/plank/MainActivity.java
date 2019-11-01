f0663f083d94fb06f5d2e8bb97991ffa30af0dc3
Kouki Nishihara <e175774@ie.u-ryukyu.ac.jp>
2019-11-02 08:08:35 +0900

    MainActivityのコメント整理

--- app/src/main/java/com/example/plank/MainActivity.java
+++ app/src/main/java/com/example/plank/MainActivity.java
@@ -1,24 +1,14 @@
 package com.example.plank;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.net.Uri;
 import android.os.Bundle;
-import java.io.File;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private final static int RESULT_CAMERA = 1001;
-    private final static int REQUEST_PERMISSION = 1002;//10/2追加
-    private ImageView imageView;
-    private ImageView imageView2;
-    private Uri cameraUri;//10/2追加
-    private File cameraFile;//10/2追加
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,39 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("debug","onCreate()");//10/2追加
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.image_view);
-        imageView2 = findViewById(R.id.image_view2);
-
-        /*
-        Button sendButton = findViewById(R.id.movie_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), MovieActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        //カメラボタン
-        Button cameraButton = findViewById(R.id.camera_button);
-        cameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //追加
-                // Android 6, API 23以上でパーミッシンの確認
-                if (Build.VERSION.SDK_INT >= 23) {
-                    checkPermission();
-                }
-                else {
-                    cameraIntent();
-                }
-
-            }
-        });
-
-         */
-
+        //カメラモードに移動
         Button PhotoButton = findViewById(R.id.ViewImg);
         PhotoButton.setOnClickListener(new View.OnClickListener() {
 
@@ -69,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
                 }
 
         });
-
 
         //センサーようの移動ボタン
         Button sendButton_sensor = findViewById(R.id.sensor_button);
