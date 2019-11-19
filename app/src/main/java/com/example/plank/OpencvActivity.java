8c11899bde19f1e10c3ef3ad8e0222a3469286e3
e175751@ie.u-ryukyu.ac.jp <e175751 e175751@ie.u-ryukyu.ac.jp>
2019-11-19 23:32:28 +0900

    add OpencvActivity.java callback

--- app/src/main/java/com/example/plank/OpencvActivity.java
+++ app/src/main/java/com/example/plank/OpencvActivity.java
@@ -1,13 +1,45 @@
 package com.example.plank;
 
-import androidx.appcompat.app.AppCompatActivity;
+//androidのモジュール
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
+//opencvのモジュール
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
-public class OpencvActivity extends AppCompatActivity{
+public class OpencvActivity extends Activity {
 
+
+    // CameraBridgeViewBase は JavaCameraView/NativeCameraView のスーパークラス
+    private CameraBridgeViewBase mCameraView;
+
+    // ライブラリ初期化完了後に呼ばれるコールバック (onManagerConnected)
+    // public abstract class BaseLoaderCallback implements LoaderCallbackInterface
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                // 読み込みが成功したらカメラプレビューを開始
+                case LoaderCallbackInterface.SUCCESS:
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
