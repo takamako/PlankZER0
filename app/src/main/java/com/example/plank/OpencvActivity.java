0a99635942da8691e99a9762adc686851f393fe7
e175751@ie.u-ryukyu.ac.jp <e175751 e175751@ie.u-ryukyu.ac.jp>
2019-11-23 10:16:32 +0900

    camera does not start

--- app/src/main/java/com/example/plank/OpencvActivity.java
+++ app/src/main/java/com/example/plank/OpencvActivity.java
@@ -4,6 +4,7 @@ package com.example.plank;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.util.Log;
 
 
 //opencvのモジュール
@@ -15,8 +16,9 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Point;
 
-public class OpencvActivity extends Activity {
+public class OpencvActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener{
 
 
     // CameraBridgeViewBase は JavaCameraView/NativeCameraView のスーパークラス
@@ -25,12 +27,20 @@ public class OpencvActivity extends Activity {
     // ライブラリ初期化完了後に呼ばれるコールバック (onManagerConnected)
     // public abstract class BaseLoaderCallback implements LoaderCallbackInterface
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+
         @Override
         public void onManagerConnected(int status) {
+            Log.d("tag","onManagerConnected・status="+status);
             switch (status) {
                 // 読み込みが成功したらカメラプレビューを開始
                 case LoaderCallbackInterface.SUCCESS:
+
+                    Log.d("tag","読み込みが成功したらカメラプレビューを開始");
+                    //mCameraView = findViewById(R.id.camera_view);
+                    //mCameraView.setVisibility(SurfaceView.VISIBLE);
+                    //mCameraView.setCvCameraViewListener(OpencvActivity.this);
                     mCameraView.enableView();
+                    //mCameraView.enableFpsMeter();
                     break;
                 default:
                     super.onManagerConnected(status);
@@ -39,13 +49,60 @@ public class OpencvActivity extends Activity {
         }
     };
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opencv);
-
-        TextView textView = findViewById(R.id.text1);
-        textView.setText(OpenCVLoader.OPENCV_VERSION);  // ★OpenCVのバージョンを設定
+        Log.d("camera","onCreate");
+        // カメラビューのインスタンスを変数にバインド
+        mCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        // リスナーの設定 (後述)
+        mCameraView.setCvCameraViewListener(this);
     }
+/**
+    public static boolean getPermissionCamera(Activity activity){
+        if (ContextCompat.checkSelfPermission(activity, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = new String[]{Manifest.permission.CAMERA};
+            ActivityCompat.requestPermissions(activity, permissions, 0);
+            return false;
+        } else {
+            return true;
+        }
+    }
+ */
+
+    //必須
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 非同期でライブラリの読み込み/初期化を行う
+        // static boolean initAsync(String Version, Context AppContext, LoaderCallbackInterface Callback)
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("onResume", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this, mLoaderCallback);
+        } else {
+            Log.d("onResume", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        // カメラプレビュー開始時に呼ばれる
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        // カメラプレビュー終了時に呼ばれる
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        return null;
+    }
+
+
+
 }
