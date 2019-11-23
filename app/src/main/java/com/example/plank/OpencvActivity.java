a31c47e685a7d1eed755c4b686de458fb73c38ed
e175751@ie.u-ryukyu.ac.jp <e175751 e175751@ie.u-ryukyu.ac.jp>
2019-11-23 17:28:15 +0900

    modified opencv_activity

--- app/src/main/java/com/example/plank/OpencvActivity.java
+++ app/src/main/java/com/example/plank/OpencvActivity.java
@@ -15,110 +15,75 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.core.Mat;
+import org.opencv.core.Core;
 
 
-public class OpencvActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener{
 
-    // CameraBridgeViewBase は JavaCameraView/NativeCameraView のスーパークラス
-    private CameraBridgeViewBase mCameraView;
-    private Mat mOutputFrame;
+public class OpencvActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener {
+
+    private CameraBridgeViewBase m_cameraView;
 
     static {
         System.loadLibrary("opencv_java4");
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opencv);
+
+        //OpenCVLoader.initDebug();
         getPermissionCamera(this);
         Log.d("camera","get permission");
+
         // カメラビューのインスタンスを変数にバインド
-        mCameraView = findViewById(R.id.camera_view);
+        m_cameraView = findViewById(R.id.camera_view);
+
         // リスナーの設定 (後述)
-        mCameraView.setCvCameraViewListener(this);
+        m_cameraView.setCvCameraViewListener(this);
+
+
+        m_cameraView.enableView();
     }
 
-    // ライブラリ初期化完了後に呼ばれるコールバック (onManagerConnected)
-    // public abstract class BaseLoaderCallback implements LoaderCallbackInterface
-
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-
-        @Override
-        public void onManagerConnected(int status) {
-            Log.d("tag","onManagerConnected・status="+status);
-            switch (status) {
-                // 読み込みが成功したらカメラプレビューを開始
-                case LoaderCallbackInterface.SUCCESS:
-                    Log.d("tag","読み込みが成功したらカメラプレビューを開始");
-                    mCameraView.enableView();
-                    break;
-                default:
-                    super.onManagerConnected(status);
-                    break;
-            }
-        }
-    };
-
-    public static boolean getPermissionCamera(Activity activity){
-        if (ContextCompat.checkSelfPermission(activity, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+    public static boolean getPermissionCamera(Activity activity) {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
             String[] permissions = new String[]{Manifest.permission.CAMERA};
-            ActivityCompat.requestPermissions(activity, permissions, 0);
+            ActivityCompat.requestPermissions(
+                    activity,
+                    permissions,
+                    0);
             return false;
         } else {
             return true;
         }
     }
 
-    //必須
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // 非同期でライブラリの読み込み/初期化を行う
-        if (!OpenCVLoader.initDebug()) {
-            Log.d("onResume", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-        } else {
-            Log.d("onResume", "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
 
-
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mCameraView.disableView();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mCameraView != null) {
-            mCameraView.disableView();
-        }
-    }
-
-
-    //カメラビュー開始時の処理
     @Override
     public void onCameraViewStarted(int width, int height) {
-        // カメラプレビュー開始時に呼ばれる
+
     }
 
-
-    //カメラビュー終了時の処理
     @Override
     public void onCameraViewStopped() {
-        // カメラプレビュー終了時に呼ばれる
+
     }
 
-    //画像処理するメソッド
     @Override
     public Mat onCameraFrame(Mat inputFrame) {
-        return null;
+        // ここで何らかの画像処理を行う
+        // 試しに、ネガポジ反転してみる
+        Core.bitwise_not(inputFrame, inputFrame);
+        return inputFrame;
     }
 
 
 }
+
+
+
