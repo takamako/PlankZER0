b132ace4fe3d02bba049323ba2c9511f28c71643
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2019-11-26 17:36:27 +0900

    ヘルプの表示　画像の埋め込みが途中

--- app/src/main/java/com/example/plank/SensorActivity.java
+++ app/src/main/java/com/example/plank/SensorActivity.java
@@ -2,11 +2,21 @@ package com.example.plank;
 
 //AndroidX
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ImageView;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import androidx.annotation.NonNull;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 
 
 public class SensorActivity extends AppCompatActivity {
@@ -58,5 +68,61 @@ public class SensorActivity extends AppCompatActivity {
             }
         });
 
+
+
+        Button helpButton = findViewById(R.id.helpButton);
+
+        // ボタンタップでAlertを表示させる
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+
+                // DialogFragment を継承したAlertDialogFragmentのインスタンス
+                AlertDialogFragment dialogFragment = new AlertDialogFragment();
+                // DialogFragmentの表示
+                dialogFragment.show(fragmentManager, "test alert dialog");
+            }
+        });
+    }
+
+    public void setTextView(String message){
+
+    }
+
+    // DialogFragment を継承したクラス
+    public static class AlertDialogFragment extends DialogFragment {
+        // 選択肢のリスト
+        private String[] menulist = {"選択(1)","選択(2)","選択(3)"};
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        @NonNull
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            ImageView imageView = new ImageView(getContext());
+            imageView.setImageResource( R.drawable.sample );
+            // タイトル
+            alert.setTitle("使い方！");
+            alert.setView(  imageView );
+            alert.setPositiveButton( "OK", null );
+            //alert.show();
+            //alert.setItems(menulist, new DialogInterface.OnClickListener() {
+               // @Override
+                //public void onClick(DialogInterface dialog, int idx) {
+
+                //    }
+                //}
+           // });
+
+            return alert.create();
+        }
+
+        private void setMassage(String message) {
+            MainActivity mainActivity = (MainActivity) getActivity();
+          // mainActivity.setTextView(message);
+        }
     }
 }
