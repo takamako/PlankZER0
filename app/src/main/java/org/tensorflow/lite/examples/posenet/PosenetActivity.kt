1153f1eb44300832380bdc211c9fc34d10043788
KoukiNishihara <e175774@ie.u-ryukyu.ac.jp>
2019-12-31 01:47:37 +0900

    2019最後のcommit

--- app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
@@ -59,6 +59,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import java.util.concurrent.Semaphore
 import java.util.concurrent.TimeUnit
