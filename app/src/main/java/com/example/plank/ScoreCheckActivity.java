6131fad77d8f681dee77b09c72108414d3efa670
Takumi Takamatu <e175744@ie.u-ryukyu.ac.jp>
2020-01-07 00:32:50 +0900

    一人ようにスコアチェック用の画面への遷移ボタンを追加

--- app/src/main/java/com/example/plank/ScoreCheckActivity.java
+++ app/src/main/java/com/example/plank/ScoreCheckActivity.java
@@ -1,4 +1,15 @@
 package com.example.plank;
 
-public class ScoreCheckActivity {
+import android.os.Bundle;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ScoreCheckActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scorecheck);
+    }
 }
