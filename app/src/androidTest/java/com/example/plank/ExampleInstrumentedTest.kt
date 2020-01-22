a5723c8359a7c20f6ab4eabe3106db215793d451
KoukiNishihara <e175774@ie.u-ryukyu.ac.jp>
2020-01-23 01:49:52 +0900

    Test.ktでベンチマークしようとしてる

---
+++ app/src/androidTest/java/com/example/plank/ExampleInstrumentedTest.kt
@@ -0,0 +1,37 @@
+package com.example.plank
+
+import android.content.Context
+import androidx.benchmark.junit4.BenchmarkRule
+import androidx.benchmark.junit4.measureRepeated
+
+import androidx.test.*
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @get:Rule
+    val benchmarkRule = BenchmarkRule()
+
+    @Test
+//    fun useAppContext() {
+        fun benchmarkSomeWork() = benchmarkRule.measureRepeated {
+
+
+    // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        assertEquals("com.example.plank", appContext.packageName)
+    }
+}
