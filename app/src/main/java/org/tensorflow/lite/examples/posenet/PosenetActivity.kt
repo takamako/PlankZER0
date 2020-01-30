5381c21897839d2b6c1fbe4ca1452d5a5af2c9ad
KoukiNishihara <e175774@ie.u-ryukyu.ac.jp>
2020-01-31 02:22:32 +0900

    フロントカメラを起動するようにした

--- app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
@@ -288,8 +288,10 @@ class PosenetActivity :
         // We don't use a front facing camera in this sample.
         val cameraDirection = characteristics.get(CameraCharacteristics.LENS_FACING)
         if (cameraDirection != null &&
-          cameraDirection == CameraCharacteristics.LENS_FACING_FRONT
-        ) {
+          //cameraDirection == CameraCharacteristics.LENS_FACING_FRONT
+            cameraDirection == CameraCharacteristics.LENS_FACING_BACK
+
+                ) {
           continue
         }
 
