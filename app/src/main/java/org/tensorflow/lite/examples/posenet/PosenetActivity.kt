97791d89627a1d74a40172ae3d7f52684df12bcc
KoukiNishihara <e175774@ie.u-ryukyu.ac.jp>
2020-02-13 03:01:37 +0900

    ボタン色変更

--- app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/PosenetActivity.kt
@@ -291,8 +291,8 @@ class PosenetActivity :
         // We don't use a front facing camera in this sample.
         val cameraDirection = characteristics.get(CameraCharacteristics.LENS_FACING)
         if (cameraDirection != null &&
-          cameraDirection == CameraCharacteristics.LENS_FACING_FRONT
-//            cameraDirection == CameraCharacteristics.LENS_FACING_BACK
+//          cameraDirection == CameraCharacteristics.LENS_FACING_FRONT
+            cameraDirection == CameraCharacteristics.LENS_FACING_BACK
 
                 ) {
           continue
